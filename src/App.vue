21b5ecbeb15d6d3cfbbf28fa8c6c7c556684b332
Jip Irfandy <jip.irfandy@dayadimensi.co.id>
2021-06-21 21:44:37 +0700

    feat(App.vue): add ability to add 2 markers

--- src/App.vue
+++ src/App.vue
@@ -3,12 +3,19 @@
     <Map
       :zoom="mapConfig.zoom"
       :center="mapConfig.center"
+      @click="onMapClick"
       class="app__map"
     >
       <TileLayer
         :url="mapConfig.url"
         :attribution="mapConfig.attribution"
       />
+
+      <LMarker v-for="(mark, i) in markers"
+        :key="i"
+        :lat-lng.sync="mark.position"
+      >
+      </LMarker>
 
     </Map>
   </div>
@@ -21,14 +28,30 @@ export default {
   name: 'App',
   components: {
     Map: require('./components/atoms/LeafletMap').default,
-    TileLayer: require('./components/atoms/LeafletTileLayer').default
+    TileLayer: require('./components/atoms/LeafletTileLayer').default,
+    LMarker: require('./components/atoms/LeafletMarker').default
   },
   data () {
     return {
       mapConfig: {
         zoom: 10,
         center: latLng( -6.200000, 106.816666)
+      },
+      markers: []
+    }
+  },
+  methods: {
+    onMapClick (e) {
+      this.addMarker(e.latlng)
+    },
+    addMarker(position) {
+      if (this.markers.length > 1) {
+        this.markers = []
       }
+
+      this.markers.push({
+        position
+      })
     }
   }
 };
